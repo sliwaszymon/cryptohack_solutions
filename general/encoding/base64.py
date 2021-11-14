b9577d853a4ac65abbacc31af533a40549292219
szymon242820 <58854456+szymon242820@users.noreply.github.com>
2021-11-14 18:26:09 +0100

    refactor

--- general/encoding/base64.py
+++ general/encoding/base64.py
@@ -1,9 +1,8 @@
 import base64
-
 hex = '72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf'
-decoded = bytes.fromhex(hex)
-print(decoded)
-decoded = base64.b64encode(decoded)
+bytes_from_hex = bytes.fromhex(hex)
+print(bytes_from_hex)
+decoded = base64.b64encode(bytes_from_hex)
 print(decoded)
 
 # flag: crypto/Base+64+Encoding+is+Web+Safe/
