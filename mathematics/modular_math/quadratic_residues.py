6ee2d28ebd243c776d0927d58b8106821b09ef65
szymon242820 <58854456+szymon242820@users.noreply.github.com>
2021-11-15 15:48:34 +0100

    Update quadratic_residues.py

--- mathematics/modular_math/quadratic_residues.py
+++ mathematics/modular_math/quadratic_residues.py
@@ -1,8 +1,8 @@
 p = 29
 ints = [14, 6, 11]
 
-ans = [x for x in range(p) if (x**2 % p) in ints]
-answer = min(ans)
+answers = [x for x in range(1, p, 1) if (x**2 % p) in ints]
+answer = min(answers)
 print(answer)
 
 # flag: 8
