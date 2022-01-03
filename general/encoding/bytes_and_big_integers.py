ab657549be428ba1071fd3c728e49d8f82f7bfff
szymon242820 <58854456+szymon242820@users.noreply.github.com>
2022-01-03 15:41:09 +0100

    crypto_on_the_web
    rsa_or_hmac done

--- general/encoding/bytes_and_big_integers.py
+++ general/encoding/bytes_and_big_integers.py
@@ -9,5 +9,4 @@ import Crypto.Util.number
 integer_msg = 11515195063862318899931685488813747395775516287289682636499965282714637259206269
 decoded = Crypto.Util.number.long_to_bytes(integer_msg)
 print(decoded)
-
 # flag: crypto{3nc0d1n6_4ll_7h3_w4y_d0wn}
